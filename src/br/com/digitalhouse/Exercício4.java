2570744f4038386b0453f9c35e3db48e6dd00c79
ytifit <fiygfgh>
2019-04-03 22:44:14 -0300

    Alteração For

--- src/br/com/digitalhouse/Exercício4.java
+++ src/br/com/digitalhouse/Exercício4.java
@@ -2,15 +2,14 @@ package br.com.digitalhouse;
 
 public class Exercício4 {
     public static void main(String[] args) {
-        int [] numeros = new int[4];
+        int[] numeros = new int[4];
         numeros[0] = 1;
         numeros[1] = 23;
         numeros[2] = 2;
-        numeros [3] = 12;
+        numeros[3] = 12;
 
-        for (int i = 0; i < numeros, i++)}{
-        System.out.println(numeros(i));
-
-
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
     }
 }
