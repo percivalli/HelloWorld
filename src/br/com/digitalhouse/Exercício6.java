4113f44607a0920532176b467234eaa31797910d
percivalli <thamuz.khaos@gmail.com>
2019-04-05 14:58:58 -0300

    Tarefa1

---
+++ src/br/com/digitalhouse/Exercício6.java
@@ -0,0 +1,4 @@
+package br.com.digitalhouse;
+
+public class Exercício6 {
+}
---
+++ src/br/com/digitalhouse/Tarefa1.java
@@ -0,0 +1,14 @@
+package br.com.digitalhouse;
+
+public class Tarefa1 {
+    public static void main(String[] args)
+        {
+        float umNumeroA = 10;
+        float umNumeroB = 20;
+        float umNumeroC = 30;
+
+        System.out.println("O maior dos três números é: ." +Math.max(umNumeroA, umNumeroB, umNumeroC));
+
+    }
+
+}
