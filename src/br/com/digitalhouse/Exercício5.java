5dd0ee2883873aeb180ae73c37f6b6104c242b49
percivalli <thamuz.khaos@gmail.com>
2019-04-05 14:26:59 -0300

    Exercício5

---
+++ src/br/com/digitalhouse/Exercício5.java
@@ -0,0 +1,11 @@
+package br.com.digitalhouse;
+
+public class Exercício5 {
+    public static void main(String[] args) {
+        int [] numerosPares = new int [10];
+        for (int i = 0; i > numerosPares.length; i++) {
+            System.out.println(numerosPares);
+        }
+
+    }
+}
